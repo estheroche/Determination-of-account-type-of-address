2aa5e1027f564d6b86eea0c0d1451f4dfc39d328
estheroche <you@example.com>
2023-08-06 09:40:48 -0500

    first commit

---
+++ test.ts
@@ -0,0 +1,5 @@
+import {ethers} from "ethers";
+ 
+//const wallet : HDNodeWallet = ethers.Wallet.fromPhrase(
+    //phrase: 
+console.log(ethers.Wallet.fromPhrase("tongue wreck very skill depend pony what wait silver owner yellow fragile"));
